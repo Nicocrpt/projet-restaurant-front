d6f10ae64b9b71d6069f40ed4fcdc3420d2c1de7
lmeline <loisemel@hotmail.fr>
2026-07-03 13:23:12 +0200

    modification carte

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ function Header() {
   const navigate = useNavigate();
   const { isAuthenticated, isAdmin, user, logout } = useAuth();
 
-  const handleLogout = () => {
-    logout();
+  const handleLogout = async  () => {
+    await logout();
     navigate("/");
   };
 
@@ -24,9 +24,11 @@ function Header() {
           <Link to="/menu" className="nav-link">
             La Carte
           </Link>
-           <Link to="/login" className="nav-link">
-            Reserver une table
-          </Link>
+           {!isAuthenticated && (
+            <Link to="/login" className="nav-link">
+              Réserver une table
+            </Link>
+          )}
           {isAuthenticated && (
             <Link to="/my-reservations" className="nav-link">
               Mes Réservations
